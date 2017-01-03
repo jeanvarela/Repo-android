f4cb8bfd704ef7f79cd7cf2384f2c75f19d70a62
jean <jean.varela1@gmail.com>
2017-01-03 01:34:31 -0200

    Criado o menu no toolbar

--- Toolbar/app/src/main/java/br/com/toolbar/MainActivity.java
+++ Toolbar/app/src/main/java/br/com/toolbar/MainActivity.java
@@ -1,13 +1,124 @@
 package br.com.toolbar;
 
+import android.content.res.Configuration;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ActionBarActivity implements AdapterView.OnItemClickListener{
+
+    private DrawerLayout mDrawerLayout;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private Toolbar mToolbar;
+    private ListView mDrawerList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        mDrawerToggle = new ActionBarDrawerToggle(this,mDrawerLayout,mToolbar,R.string.app_name,R.string.app_name){
+            @Override
+            public void onDrawerClosed(View drawerView) {
+                super.onDrawerClosed(drawerView);
+                supportInvalidateOptionsMenu();
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                supportInvalidateOptionsMenu();
+            }
+        };
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,getResources().getStringArray(R.array.opcoes));
+        mDrawerList.setAdapter(adapter);
+        mDrawerList.setOnItemClickListener(this);
+
+        if (savedInstanceState == null){
+            exibirItem(0);
+        }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        exibirItem(i);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
+
+         menu.findItem(R.id.action_settings).setVisible(!drawerOpen);
+        atualizarTitulo(drawerOpen);
+
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mDrawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void exibirItem(int i){
+        String selecionado = mDrawerList.getItemAtPosition(i).toString();
+        Fragment fragment = PrimeiroNivelFragment.novaInstancia(selecionado);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.content_frame,fragment).commit();
+
+        mDrawerList.setItemChecked(i,true);
+        mDrawerLayout.closeDrawer(mDrawerList);
+    }
+
+    private void atualizarTitulo(boolean atualizar){
+        if (atualizar){
+            mToolbar.setTitle(R.string.app_name);
+        }else{
+            int posicaoAtual = mDrawerList.getCheckedItemPosition();
+            String opcaoAtual = mDrawerList.getItemAtPosition(posicaoAtual).toString();
+            mToolbar.setTitle(opcaoAtual);
+        }
     }
 }
