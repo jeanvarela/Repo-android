2eb93784a806d77f12c04a649504b98c85e81c28
Jean Varela <jean.varela1@gmail.com>
2017-05-11 02:27:01 -0300

    Adicionado o projeto Listview. Esse projeto exemplifica a utilização da Listview

---
+++ Listview/app/src/main/java/br/com/listview/MainActivity.java
@@ -0,0 +1,40 @@
+package br.com.listview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView lista = (ListView)findViewById(R.id.lista);
+
+        lista.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this,"Click simples",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        lista.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this,"Click longo",Toast.LENGTH_LONG).show();
+                return true;
+            }
+        });
+
+        String[] conteudo = {"Alemanha","Brasil","EUA"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,conteudo);
+        lista.setAdapter(adapter);
+    }
+}
